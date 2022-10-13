0719513b3cec1ffbcf8b531b9f09d7ae23749d4b
jacuela <jacuela@gmail.com>
2022-10-13 12:20:42 +0200

    primer commit

---
+++ src/iesramonarcas/Ejercicio00_Iniciacion.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iesramonarcas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Antonio Cuello Alarcon
+ */
+public class Ejercicio00_Iniciacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado=new Scanner (System.in);
+        
+        
+//        System.out.println("**** for ****");
+//        for (int i=1;i<=10;i++){
+//            System.out.println("i vale:"+i);
+//            
+//        }
+//     
+//        
+//        System.out.println("\n\n**** while ****");
+//        int i=1;
+//        while(i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+//        
+//        System.out.println("\n\n**** do while ****");
+//        i=1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while(i<11);
+        
+        
+         
+//        System.out.println("\n\n**** Introducir numeros hasta negativ ****");
+//        
+//        int num;
+//        
+//        System.out.println("--- Usando un while ---");
+//        System.out.print("Numero positivo para continuar: ");
+//        num=Integer.parseInt(teclado.nextLine());
+//
+//        while (num>=0){
+//            System.out.print("Numero positivo para continuar: ");
+//            num=Integer.parseInt(teclado.nextLine());
+//        }
+//        
+//        
+//        System.out.println("--- Usando un do..while ---");
+//        do{
+//            System.out.print("Numero positivo para continuar: ");
+//            num=Integer.parseInt(teclado.nextLine());
+//        } while (num>=0);
+        
+        
+        
+
+//          //Introducir numeros por teclado hasta impar
+//          System.out.println("**** Introducir hasta impar ****");
+//          int num;
+//          
+//          do{
+//            System.out.print("Numero par para continuar: ");
+//            num=Integer.parseInt(teclado.nextLine());
+//          }while (num%2==0);
+//         
+//          //int num;
+//          
+//          System.out.print("Numero par para continuar: ");
+//          num=Integer.parseInt(teclado.nextLine());
+//          
+//          while(num%2==0){
+//            System.out.print("Numero par para continuar: ");
+//            num=Integer.parseInt(teclado.nextLine());
+//          }
+         
+
+          //Leer pares parando con un break
+//          System.out.println("**** Introducir hasta impar ****");
+//          int num;
+//          
+//          do{
+//            System.out.print("Numero par para continuar: ");
+//            num=Integer.parseInt(teclado.nextLine());
+//            if (num%2!=0){
+//                break;
+//            }
+//          }while (true);
+           
+          //Usar un for con break. Leer 10 numeros y parar si pongo un impar
+//          int num;
+//          for (int i=0;i<10;i++){
+//                //System.out.print("Paso "+i+"-->");
+//                System.out.print("PAR para continuar: ");
+//                num=Integer.parseInt(teclado.nextLine());
+//                if (num%2!=0){
+//                    break;
+//                }
+//          }
+          
+//        //Imprimir letras de la A a la Z  
+//        for (int i=65;i<=90;i++){
+//            System.out.print("Letra:"+(char)i);
+//            System.out.println("---"+(char)(i+32));
+//           
+//        }
+//     
+            
+            
+            
+
+//            //COntadores y acumuladores  
+//            int num=0;
+//            int suma=0;
+//            int contador=0;
+//
+//            while (num>=0){
+//                System.out.println("Introduce numero: ");
+//                num=Integer.parseInt(teclado.nextLine());
+//                
+//                if (num>=0){
+//                    suma=suma+num;   //añado al acumulador
+//                    contador++;  //incremento contador
+//                }
+//            }
+//            System.out.println("La suma acumulada es:"+suma);
+//            System.out.println("Total numeros introduc:"+contador);
+
+    
+        
+        
+    }
+    
+}
---
+++ src/iesramonarcas/Ejercicio01.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iesramonarcas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jacuela
+ */
+public class Ejercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=new Scanner (System.in);
+        int num;
+        
+        System.out.print("Dime un numero:");
+        num=Integer.parseInt(entrada.nextLine());
+        
+        System.out.println("\n\nTabla de multiplicar del número "+num);
+        
+        System.out.println("USANDO FOR");
+        System.out.println("==========");
+        for (int i=0;i<=10;i++){
+        
+            System.out.println(num+"*"+i+"="+(num*i));
+            
+        }
+        
+        System.out.println("USANDO while");
+        System.out.println("==========");
+        int j=0;
+        while (j<=10){
+                System.out.println(num+"*"+j+"="+(num*j));
+                j++;
+        }
+       
+        //En este punto, j vale 11
+        
+        System.out.println("USANDO do..while");
+        System.out.println("==========");
+        
+        j=0;
+        do{
+             System.out.println(num+"*"+j+"="+(num*j));
+                j++;
+        }while(j<=10);
+        
+        
+        
+        
+        String numero="123456";
+        System.out.println("Tamaño del string:"+numero.length());
+        System.out.println();
+        
+        
+        
+    }
+    
+}
