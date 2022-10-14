1b6fd71d892b12b609e3d38f9cf733b0e6d746b4
jacuela <jacuela@gmail.com>
2022-10-14 08:48:57 +0200

    bucles hasta el 5

---
+++ src/iesramonarcas/Ejercicio03_Contadores.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iesramonarcas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jacuela
+ */
+public class Ejercicio03_Contadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=new Scanner(System.in);
+        
+        
+        
+//        //*********  USANDO UN DO....WHILE  ***********
+//        
+//        System.out.println("[do..while]Introduce un número y pulsa INTRO. 0 para terminar\n");
+//
+//        int numeroIntroducido; 
+//        int cuentaNumeros = 0;    //mi contador
+//        int suma = 0;             //mi acumulador
+//
+//        do{
+//            
+//            System.out.print("Dime el numero:");
+//            numeroIntroducido = Integer.parseInt(entrada.nextLine()); 
+//            cuentaNumeros++; // Incrementamos el contador
+//            suma = suma + numeroIntroducido; //Acumulamos suma parcial
+//        
+//        } while (numeroIntroducido != 0 ); 
+//        
+//        System.out.println("\nHas introducido " + (cuentaNumeros-1) + " números.");
+//        System.out.println("La suma total de ellos es " + suma);
+//        
+//        
+
+
+        //*********  USANDO UN WHILE  ***********
+        
+        System.out.println("[while] Introduce un número y pulsa INTRO. 0 para terminar\n");
+
+
+        //Inicializamos variables
+        int cuentaNumeros = 0;    //mi contador
+        int suma = 0; 
+        int numeroIntroducido=6666666;
+        
+        //OPCIONAL: Tratamiento especial del primer elemento
+//        System.out.print("Dime el numero:");
+//        numeroIntroducido = Integer.parseInt(entrada.nextLine()); 
+//        suma = suma + numeroIntroducido;
+//        cuentaNumeros++;
+        
+
+
+        while(numeroIntroducido != 0 ){
+            System.out.print("Dime el numero:");
+            numeroIntroducido = Integer.parseInt(entrada.nextLine()); 
+            cuentaNumeros++; // Incrementamos el contador
+            suma = suma + numeroIntroducido; //Acumulamos suma parcial
+        }
+        System.out.println("\nHas introducido " + (cuentaNumeros-1) + " números.");
+        System.out.println("La suma total de ellos es " + suma);
+        
+        
+        
+        
+    }
+    
+}
---
+++ src/iesramonarcas/Ejercicio04.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iesramonarcas;
+
+/**
+ *
+ * @author jacuela
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i;
+        
+//        System.out.println("Multiplos de 5 usando un for");
+//        for(i = 0; i <= 50;i=i+5) { 
+//            System.out.println(i);
+//        }
+//        
+//        
+//        System.out.println("Multiplos de 5 usando un while");
+//        i = 0;
+//        while(i <= 50) { 
+//            System.out.println(i); 
+//            i=i+5;
+//        }
+//        
+//        System.out.println("Multiplos de 5 usando un do-while");
+//        i = 0;
+//        do { 
+//            System.out.println(i); 
+//            i+=5;
+//        } while(i <= 50);
+
+
+        //Numeros del 100 al 50
+        for (int j = 100; j >= 50; j--) {
+            
+            if (j%2 == 0) {
+                System.out.println(j);
+            }
+        }
+        
+        System.out.println("Numeros del 100 al 50 pares con un while");
+        i=100;
+        while(i>=50){
+            if (i%2==0) System.out.println(i);
+            i--;
+        }
+       
+        
+        
+    }
+    
+}
