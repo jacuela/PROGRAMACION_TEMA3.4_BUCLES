17cb2f35ae0baa4a2706f08b581f08822b860a08
jacuela <jacuela@gmail.com>
2023-10-05 14:10:14 +0200

    jueves - HASTA EL 5

---
+++ src/iesramonarcas/Ejercicio03_Contadores.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iesramonarcas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jacuela
+ */
+public class Ejercicio03_Contadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=new Scanner(System.in);
+        
+        
+        
+        //*********  USANDO UN DO....WHILE  ***********
+        
+        System.out.println("[do..while]Introduce un número y pulsa INTRO. 0 para terminar\n");
+
+        int numeroIntroducido; 
+        int cuentaNumeros = 0;    //mi contador
+        int suma = 0;             //mi acumulador
+
+        do{
+            
+            System.out.print("Dime el numero:");
+            numeroIntroducido = Integer.parseInt(entrada.nextLine()); 
+            if (numeroIntroducido!=0){
+                cuentaNumeros++; // Incrementamos el contador
+                suma = suma + numeroIntroducido; //Acumulamos suma parcial
+            }
+            
+              
+        } while (numeroIntroducido != 0 ); 
+        
+        System.out.println("\nHas introducido " + (cuentaNumeros) + " números.");
+        System.out.println("La suma total de ellos es " + suma);
+        
+        
+
+
+        //*********  USANDO UN WHILE  ***********
+        
+        System.out.println("[while] Introduce un número y pulsa INTRO. 0 para terminar\n");
+
+
+        //Inicializamos variables
+        cuentaNumeros = 0;    //mi contador
+        suma = 0; 
+        numeroIntroducido=-1; //para poder entrar al while. Del anterior, se quedaba en 0 y no entraba 
+        
+       
+        while(numeroIntroducido != 0 ){
+            System.out.print("Dime el numero:");
+            numeroIntroducido = Integer.parseInt(entrada.nextLine()); 
+           if (numeroIntroducido!=0){
+                cuentaNumeros++; // Incrementamos el contador
+                suma = suma + numeroIntroducido; //Acumulamos suma parcial
+            }
+        }
+        System.out.println("\nHas introducido " + (cuentaNumeros) + " números.");
+        System.out.println("La suma total de ellos es " + suma);
+        
+        
+        
+        
+    }
+    
+}
