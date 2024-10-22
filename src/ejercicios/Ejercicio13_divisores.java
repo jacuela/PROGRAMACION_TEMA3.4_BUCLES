aed8f505343e7a5755504b5c9cbdaf048a1f9f3a
jacuela <jacuela@gmail.com>
2024-10-22 17:30:11 +0200

    martes

--- src/ejercicios/Ejercicio13_divisores.java
+++ src/ejercicios/Ejercicio13_divisores.java
@@ -19,7 +19,6 @@ public class Ejercicio13_divisores {
         // TODO code application logic here
         Scanner teclado=new Scanner(System.in);
         int num;   //***EXPLICAR PORQUE NO PUEDO DECLARARLO DENTRO DEL DO..WHILE
-        int contadorDivisores=0;
         
         
         
@@ -31,28 +30,18 @@ public class Ejercicio13_divisores {
         
             System.out.print("Dime un numero (0 para salir):");
             num=Integer.parseInt(teclado.nextLine());
-            contadorDivisores=0;
-
+            
             for (int i = 1; i <= num; i++) {  //i son los posibles divisores
                 if (num%i==0){
                     System.out.println("  El numero "+i+" es divisor de "+num);
-                    contadorDivisores++;
                 }
             }
             System.out.println("");
-            //System.out.println("DEPURA:"+contadorDivisores);
-            if (contadorDivisores==2){
-                System.out.println("El numero "+num+" SI es PRIMO");
-            }
-            else if (num!=0){ //para evitar tratar el CERO
-                System.out.println("El numero "+num+" NO es PRIMO");
-            }
             
             
      
         }while (num!=0);    
         
-        //Sería muy fácil sacar si el numero num es primo, no?
         
         
         
